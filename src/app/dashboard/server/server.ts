16a94a89dcd3de6addd6e47e85af06a7febf9548
James Omenda <James.Omenda@ccieastafrica.com>
2025-09-11 16:32:32 +0300

    Manipulating state & using literal values

--- src/app/dashboard/server/server.ts
+++ src/app/dashboard/server/server.ts
@@ -4,8 +4,21 @@ import { Component, signal } from '@angular/core';
   selector: 'app-server',
   imports: [],
   templateUrl: './server.html',
-  styleUrl: './server.css'
+  styleUrl: './server.css',
 })
 export class Server {
- currentStatus = signal<string>('online');
+  currentStatus = signal<'online' | 'offline' | 'unknown'>('offline');
+
+  constructor() {
+    setInterval(() => {
+      const rnd = Math.random();
+      if (rnd < 0.5) {
+        this.currentStatus.set('online');
+      } else if (rnd < 0.9) {
+        this.currentStatus.set('offline');
+      } else {
+        this.currentStatus.set('unknown');
+      }
+    }, 5000);
+  }
 }
